b015c697ed38096f37366ffba6909cc5173e07fe
redon913 <rishabhc913@gmail.com>
2024-02-19 01:07:03 +0530

    tested repo

--- src/main/java/com/RestTest/restAPI/repository/cloudVendorRepository.java
+++ src/main/java/com/RestTest/restAPI/repository/cloudVendorRepository.java
@@ -1,9 +1,11 @@
 package com.RestTest.restAPI.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.RestTest.restAPI.Model.CloudVendor;
 
 public interface cloudVendorRepository extends JpaRepository<CloudVendor, String> {
-
+	List<CloudVendor> findByVendorName(String vendorName);
 }
